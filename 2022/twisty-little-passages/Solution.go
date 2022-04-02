90ed3fd10af5a416b29262613beff8ccec95de39
Victor Miclotte <victor.miclotte@gmail.com>
2022-04-03 00:38:01 +0200

    [2022/qualification/twisty-little-passages] Near solution, interactive runner passes all test cases

---
+++ 2022/twisty-little-passages/Solution.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type room struct {
+	r int
+	p int
+}
+
+func less(u, v room) bool {
+	return u.p < v.p || (u.p == v.p && u.r < v.r)
+}
+
+func main() {
+	var T int
+	fmt.Scanln(&T)
+	for t := 0; t < T; t++ {
+		var N, K int
+		fmt.Scanln(&N, &K)
+		if K >= N {
+			var R, P int
+			fmt.Scanln(&R, &P)
+			firstRoom := R
+			sum := P
+			for i := 0; i < N; i++ {
+				if i+1 == firstRoom {
+					continue
+				}
+				fmt.Printf("T %v\n", i+1)
+				fmt.Scanln(&R, &P)
+				sum += P
+			}
+			fmt.Printf("E %v\n", sum/2)
+		} else {
+			var R, P int
+			var count, sum int
+			var prevRoom, currRoom room
+			fmt.Scanln(&R, &P)
+			prevRoom = room{r: R, p: P}
+			fmt.Print("W\n")
+			fmt.Scanln(&R, &P)
+			currRoom = room{r: R, p: P}
+			if less(prevRoom, currRoom) {
+				sum += prevRoom.p
+				count += 1
+			}
+			for i := 0; i < K-2; i += 2 {
+				r := rand.Intn(N) + 1
+				fmt.Printf("T %v\n", r)
+				fmt.Scanln(&R, &P)
+				prevRoom = room{r: R, p: P}
+				fmt.Print("W\n")
+				fmt.Scanln(&R, &P)
+				currRoom = room{r: R, p: P}
+				if less(prevRoom, currRoom) {
+					sum += prevRoom.p
+					count += 1
+				}
+			}
+
+			e := int(float64(N) * float64(sum) / float64(count) / float64(2))
+			fmt.Printf("E %v\n", e)
+		}
+	}
+}
